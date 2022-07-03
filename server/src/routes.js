d6512519dbb2683dc8e149ea5864f8c94abc7609
RadinTiholov <radin.tiholov@mail.bg>
2022-07-03 17:17:19 +0300

    Create auth controller and update routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
+const authController = require('./controllers/authController.js');
+
 const router = express.Router();
+
+router.use('auth', authController);
+router.get('*', (req, res) => {
+    res.status(404).json({message: 'Not found!'});
+});
 
 module.exports = router;
