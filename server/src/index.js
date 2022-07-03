42e4dad17f59dbdd1f593f266f90e987dc5361be
RadinTiholov <radin.tiholov@mail.bg>
2022-07-03 15:30:08 +0300

    Add cors middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 
 const {PORT} = require('./config/env.js');
 const { DB_STRING }= require('./config/env.js');
+const corsMiddleware = require('./middlewares/corsMiddleware.js');
 const routes = require('./routes');
 
 const app = express();
 app.use(routes);
+app.use(express.json());
+app.use(corsMiddleware());
 
 mongoose.connect(DB_STRING)
 .then(() => {
