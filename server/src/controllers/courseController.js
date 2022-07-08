ff9bfd2f843ae0f9d65e96bb70a7bb29db287fdb
RadinTiholov <radin.tiholov@mail.bg>
2022-07-08 18:27:10 +0300

    Add like action

--- server/src/controllers/courseController.js
+++ server/src/controllers/courseController.js
@@ -84,6 +84,29 @@ router.delete('/delete/:id', isAuth, async (req, res) => {
     }
 })
 
+router.get('/like/:id', isAuth, async (req, res) => {
+    try{
+        const course = await courseService.getOne(req.params.id).lean();
+        const user = await userService.getUser(req.user._id).lean();
+        if(course){
+            if(course.owner == req.user._id || user.likedCourses.some(x => x == req.params.id)){
+                throw new Error("Unauthorized to do this action");
+            }else{
+                await courseService.likeById(req.params.id, course);
+                const userRaw = await userService.getUser(req.user._id);
+                userRaw.likedCourses.push(req.params.id);
+                userRaw.save();
+
+                res.json("Successfully liked");
+            }
+        }else{
+            throw new Error("Not found");
+        }
+    }catch(error){
+        res.status(400).json({message: error.message})
+    }
+})
+
 function ownsAll(data, owner){
     let ownsAllLessons = true;
         for (const lesson of data['lessons']) {
--- server/src/services/courseService.js
+++ server/src/services/courseService.js
@@ -5,3 +5,7 @@ exports.create = (data) => Course.create(data);
 exports.getOne = (id) => Course.findById(id);
 exports.updateById = (id, data) => Course.findOneAndUpdate({_id: id}, data, { runValidators: true });
 exports.deleteById = (id) => Course.findOneAndDelete(id);
+exports.likeById = (id, data) => {
+    data.likes += 1;
+    return Course.findOneAndUpdate({_id: id}, data, { runValidators: true })
+}
