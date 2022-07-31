289ea80ef84279723c573ab1479d3736bb0dbe4a
RadinTiholov <radin.tiholov@mail.bg>
2022-07-31 20:20:08 +0300

    Add register and lesson details components

--- client/knowledge-barter/src/App.js
+++ client/knowledge-barter/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+import { LessonDetails } from './components/LessonDetails/LessonDetails';
 import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
 
 function App() {
     return (
         <>
             <Header/>
             {/* <Home/>  */}
-            <Login/>
+            {/* <Login/> */}
+            {/* <Register/> */}
+            <LessonDetails/>
             <Footer/>
         </>
 
