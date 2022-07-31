9fa33f596035f64921318b617fc5207b8fe61f07
RadinTiholov <radin.tiholov@mail.bg>
2022-07-31 20:37:39 +0300

    Add courses and lessons components

--- client/knowledge-barter/src/App.js
+++ client/knowledge-barter/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { CourseDetails } from './components/CourseDetails/CourseDetails';
+import { Courses } from './components/Courses/Courses';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { LessonDetails } from './components/LessonDetails/LessonDetails';
+import { Lessons } from './components/Lessons/Lessons';
 import { Login } from './components/Login/Login';
 import { Profile } from './components/Profile/Profile';
 import { Register } from './components/Register/Register';
@@ -17,7 +19,9 @@ function App() {
             {/* <Register/> */}
             {/* <LessonDetails/> */}
             {/* <CourseDetails/> */}
-            <Profile/>
+            {/* <Profile/> */}
+            {/* <Lessons/> */}
+            <Courses/>
             <Footer/>
         </>
 
