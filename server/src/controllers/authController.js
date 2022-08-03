5904a95763918429a171f7e4d74dedb5ab71f83a
RadinTiholov <radin.tiholov@mail.bg>
2022-08-03 16:28:18 +0300

    Make details preview component dynamic and create auth action on the server side

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const authService = require('../services/authService.js');
+const userService = require('../services/userService.js');
 const { isAuth, isGuest } = require('../middlewares/authMiddleware.js');
 
 router.post('/login', isGuest, async (req, res) => {
@@ -25,6 +26,14 @@ router.post('/register', isGuest, async (req, res) => {
 router.get('/logout',isAuth, (req, res) => {
     authService.logout(req.user.token);
     res.status(200).json({message: "Logged out" });
+})
+router.get('/:id', async (req, res) => {
+    try{
+        const user = await userService.getUser(req.params.id).lean();
+        res.json(user);
+    }catch(error){
+        res.status(400).json({message: "Bad request"})
+    }
 })
 
 module.exports = router;
